3662677b6603243d257a8093876c64a551274e3a
ThinkDone <thinkdonee@sina.com>
2017-03-12 16:06:25 +0800

    Change the HTTP caching expiration setting

--- viewer/settings.py
+++ viewer/settings.py
@@ -93,7 +93,7 @@ IMAGES_RESULT_FIELD = 'images'
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 #HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_EXPIRATION_SECS = 60
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
